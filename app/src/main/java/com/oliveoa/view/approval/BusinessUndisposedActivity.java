4de6cf5df27f6263517713f859407555c21d628b
DementiaIsACat <m17876253479@163.com>
2018-06-14 11:54:10 +0800

    添加审批功能activity

---
+++ app/src/main/java/com/oliveoa/view/approval/BusinessUndisposedActivity.java
@@ -0,0 +1,15 @@
+package com.oliveoa.view.approval;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.oliveoa.view.R;
+
+public class BusinessUndisposedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_undisposed);
+    }
+}
