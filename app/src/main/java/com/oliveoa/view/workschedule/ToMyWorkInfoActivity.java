4f30f48c4f29c69fa84345b1359c571772e40c85
DementiaIsACat <m17876253479@163.com>
2018-09-20 10:45:27 +0800

    工作详情添加完成

--- app/src/main/java/com/oliveoa/view/workschedule/ToMyWorkInfoActivity.java
+++ app/src/main/java/com/oliveoa/view/workschedule/ToMyWorkInfoActivity.java
@@ -1,15 +1,41 @@
 package com.oliveoa.view.workschedule;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.oliveoa.view.R;
 
 public class ToMyWorkInfoActivity extends AppCompatActivity {
+    private ImageView back;
+    private TextView ttime,tcontext,tname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_my_work_info);
+
+        initView();
+    }
+
+    public void initView() {
+        back = (ImageView) findViewById(R.id.iback);
+        ttime = (TextView) findViewById(R.id.work_time);
+        tcontext = (TextView) findViewById(R.id.work_content);
+        tname = (TextView) findViewById(R.id.name);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ToMyWorkInfoActivity.this, MyWorkActivity.class);
+                intent.putExtra("index",0);
+                startActivity(intent);
+                finish();
+                //Toast.makeText(mContext, "你点击了返回", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/oliveoa/view/workschedule/WorkInfoActivity.java
+++ app/src/main/java/com/oliveoa/view/workschedule/WorkInfoActivity.java
@@ -1,15 +1,43 @@
 package com.oliveoa.view.workschedule;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.oliveoa.view.R;
 
 public class WorkInfoActivity extends AppCompatActivity {
+    private ImageView back;
+    private TextView ttime,tcontext,tadvise,tapproval,tstatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_info);
+
+        initView();
+    }
+
+    public void initView() {
+        back = (ImageView) findViewById(R.id.iback);
+        ttime = (TextView) findViewById(R.id.work_time);
+        tcontext = (TextView) findViewById(R.id.work_content);
+        tadvise = (TextView) findViewById(R.id.advise);
+        tapproval = (TextView) findViewById(R.id.person_approving);
+        tstatus = (TextView) findViewById(R.id.status);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WorkInfoActivity.this, MyWorkActivity.class);
+                intent.putExtra("index",1);
+                startActivity(intent);
+                finish();
+                //Toast.makeText(mContext, "你点击了返回", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
