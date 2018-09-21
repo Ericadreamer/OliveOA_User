4e8dc9262ca20c03cc6da77b0e7bf9824d090cee
DementiaIsACat <m17876253479@163.com>
2018-09-21 18:15:48 +0800

    公文管理页面跳转已完成

--- app/src/main/java/com/oliveoa/view/documentmanagement/NuclearDoneActivity.java
+++ app/src/main/java/com/oliveoa/view/documentmanagement/NuclearDoneActivity.java
@@ -1,15 +1,41 @@
 package com.oliveoa.view.documentmanagement;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.oliveoa.view.R;
 
-public class NuclearDoneActivity extends AppCompatActivity {
+public class NuclearDoneActivity extends Fragment {
+    private Context mContext;
+    private View rootview;
+    private RecyclerView mContentRv;
+    private TextView ttitle,tcontext;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nuclear_done);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        rootview = inflater.inflate(R.layout.activity_nuclear_done, container, false);
+        this.mContext = getActivity();
+
+        initViews();
+        initData();
+
+        return rootview;
+    }
+
+    public void initViews() {
+        ttitle = (TextView) rootview.findViewById(R.id.title);
+        tcontext = (TextView) rootview.findViewById(R.id.content);
+
+    }
+
+    public void initData() {
+
     }
 }
