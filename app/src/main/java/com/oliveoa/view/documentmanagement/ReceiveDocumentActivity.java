e5d76be4d935ec622ad43008c979545418d2ea3d
DementiaIsACat <m17876253479@163.com>
2018-09-21 10:17:55 +0800

    添加公文管理activity完成

---
+++ app/src/main/java/com/oliveoa/view/documentmanagement/ReceiveDocumentActivity.java
@@ -0,0 +1,15 @@
+package com.oliveoa.view.documentmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.oliveoa.view.R;
+
+public class ReceiveDocumentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_document);
+    }
+}
