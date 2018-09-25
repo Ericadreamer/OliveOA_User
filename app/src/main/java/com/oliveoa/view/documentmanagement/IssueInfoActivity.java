ba2fed20e2a40ed375d84f6030775dcf1cd2476c
DementiaIsACat <m17876253479@163.com>
2018-09-25 11:47:21 +0800

    签发和签收完成

--- app/src/main/java/com/oliveoa/view/documentmanagement/IssueInfoActivity.java
+++ app/src/main/java/com/oliveoa/view/documentmanagement/IssueInfoActivity.java
@@ -1,11 +1,15 @@
 package com.oliveoa.view.documentmanagement;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.oliveoa.view.R;
@@ -15,6 +19,9 @@ import java.util.TimerTask;
 
 public class IssueInfoActivity extends AppCompatActivity {
     private ImageView back;
+    private TextView ttitle,tcontent,tnuclearAdvise,tissueAdvise,tdepartment;
+    private Button btn_download;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,12 @@ public class IssueInfoActivity extends AppCompatActivity {
 
     private void initView() {
         back = (ImageView) findViewById(R.id.iback);
+        ttitle = (TextView) findViewById(R.id.title);
+        tcontent = (TextView) findViewById(R.id.content);
+        tnuclearAdvise = (TextView) findViewById(R.id.nuclear_advise);
+        tissueAdvise = (TextView) findViewById(R.id.issue_advise);
+        tdepartment = (TextView) findViewById(R.id.department);
+        btn_download = (Button) findViewById(R.id.download);
 
         //点击事件
         back.setOnClickListener(new View.OnClickListener() {  //点击返回键，返回主页
@@ -38,6 +51,33 @@ public class IssueInfoActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        btn_download.setOnClickListener(new View.OnClickListener() {  //点击返回键，返回主页
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(IssueInfoActivity.this);
+                dialog.setTitle("提示");
+                dialog.setMessage("是否下载附件");
+                dialog.setCancelable(false);
+                dialog.setNegativeButton("是", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        download();
+                    }
+                });
+                dialog.setPositiveButton("否", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+    }
+
+    public void download() {
 
     }
 
--- app/src/main/java/com/oliveoa/view/documentmanagement/ReceiveDocumentActivity.java
+++ app/src/main/java/com/oliveoa/view/documentmanagement/ReceiveDocumentActivity.java
@@ -1,11 +1,15 @@
 package com.oliveoa.view.documentmanagement;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.oliveoa.view.R;
@@ -14,7 +18,9 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class ReceiveDocumentActivity extends AppCompatActivity {
-    private ImageView back,save;
+    private ImageView back,save,readSelect;
+    private TextView ttitle,tcontent,issuePerson,readPerson;
+    private Button btn_download;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,12 @@ public class ReceiveDocumentActivity extends AppCompatActivity {
     private void initView() {
         back = (ImageView) findViewById(R.id.iback);
         save = (ImageView) findViewById(R.id.save);
+        readSelect = (ImageView) findViewById(R.id.read_select);
+        ttitle = (TextView) findViewById(R.id.title);
+        tcontent = (TextView) findViewById(R.id.content);
+        issuePerson = (TextView) findViewById(R.id.issue_person);
+        readPerson = (TextView) findViewById(R.id.read_person);
+        btn_download = (Button) findViewById(R.id.download);
 
         //点击事件
         back.setOnClickListener(new View.OnClickListener() {  //点击返回键，返回主页
@@ -45,6 +57,33 @@ public class ReceiveDocumentActivity extends AppCompatActivity {
                 save();
             }
         });
+
+        btn_download.setOnClickListener(new View.OnClickListener() {  //点击返回键，返回主页
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(ReceiveDocumentActivity.this);
+                dialog.setTitle("提示");
+                dialog.setMessage("是否下载附件");
+                dialog.setCancelable(false);
+                dialog.setNegativeButton("是", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        download();
+                    }
+                });
+                dialog.setPositiveButton("否", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+    }
+
+    public void download() {
 
     }
 
